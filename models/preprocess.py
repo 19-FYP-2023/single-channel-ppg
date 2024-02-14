6176179ecbc703ed2d2ac42a196ab61e2a51af67
Dumindu-Bandara <dumindubandara1999@gmail.com>
2024-02-14 18:16:34 +0530

    Change preprocess.py code to support relative paths

--- models/preprocess.py
+++ models/preprocess.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 data_entc = pd.read_csv("dataset/paraqum-dataset/good_signal_labels.csv")
-paths = ["dataset/paraqum-dataset/data-captures/data02052019/sensor2","dataset/paraqum-dataset/data-captures/data02052019", "dataset/paraqum-dataset/data-captures/data09012019", "dataset/paraqum-dataset/data-captures/data16012019" ]
+paths = ["dataset/paraqum-dataset/data-captures/data02052019/sensor2","dataset/paraqum-dataset/data-captures/data02052019", "dataset/paraqum-dataset/data-captures/data16012019" ]
 
 data_entc["Path"] = ""
 for index, row in data_entc.iterrows():
