dc8c0d9fd2c868c7959134b80daa9695c1b1ca7f
Dumindu-Bandara <dumindubandara1999@gmail.com>
2024-03-20 18:58:32 +0530

    Add fine-tuning functionality for SBP, DBP, and MAP

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from models.custom_dataloader import UCIBPDatasetNPY
 from models.log import readable_time
 from models.resample import resample
 from torch.optim.lr_scheduler import ExponentialLR
+from models.fine_tune import FineTuneSBP, FineTuneDBP, FineTuneMAP
 
 # logging file configuration
 logging.basicConfig(filename=f"logs/{readable_time()}.log", level=logging.INFO)
@@ -172,7 +173,94 @@ def train_ucibp(dataset="ucibp"):
             # if (epoch+1) % 5 == 0:
             #     model_save_path = f"checkpoints/vatta-pitta/PPGUnet_epoch{epoch}.pth"
             #     torch.save(model.state_dict(), model_save_path)
-     
+
+
+def train_ucibp_finetune(fine_tune_param = "sbp"):
+
+    logging.info(f"Finetunning {fine_tune_param}...")
+
+    UCIBP_dataset = UCIBPDatasetNPY("dataset/uci-bp-processed")
+    train_dataset, valid_dataset, test_dataset = random_split(UCIBP_dataset, [0.6, 0.2, 0.2], generator=torch.Generator().manual_seed(42))
+
+    train_dataloader = DataLoader(train_dataset, batch_size=128, shuffle=True)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=128, shuffle=False)
+    test_dataloader = DataLoader(test_dataset, batch_size=128, shuffle=False)
+
+    model = PPGUnet(in_channels=1).to(device)
+    model.load_state_dict(torch.load("checkpoints/uci-bp/PPGUnet_epoch299.pth"))
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+    
+    if fine_tune_param == "sbp":  
+        finetune = FineTuneSBP(1, 64, 1).to(device)
+        param_idx = 0
+
+    
+    elif fine_tune_param == "dbp":
+        finetune = FineTuneDBP(1, 64, 1).to(device)
+        param_idx = 1
+
+    elif fine_tune_param == "map":
+        finetune = FineTuneMAP(1, 64, 1).to(device)
+        param_idx = 2
+
+    aggregated_model = nn.Sequential(
+        model, 
+        finetune
+    )
+
+    num_epochs = 20
+    criterion = nn.L1Loss()
+    optimizer = torch.optim.Adam(aggregated_model.parameters(), lr=0.01)
+    scheduler = ExponentialLR(optimizer, gamma=0.992354)
+
+    for epoch in range(num_epochs):
+        aggregated_model.train()
+        train_loss = 0.0
+
+        for i, data_sample in enumerate(train_dataloader):
+
+            input, _ , pressures = data_sample
+            input = input.unsqueeze(1).to(device)  
+            target = pressures[:, param_idx].unsqueeze(1).unsqueeze(1).to(device)
+
+            optimizer.zero_grad()
+            outputs = aggregated_model(input)
+            loss = criterion(outputs, target)
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item() * input.size(0) 
+
+        train_loss = train_loss / len(train_dataloader.dataset)
+        logging.info(f"Epoch {epoch+1} / {num_epochs}, Train Loss: {train_loss:.4f}")
+
+        aggregated_model.eval()
+        valid_loss = 0.0
+
+        for i, data_sample in enumerate(valid_dataloader):
+                
+                input, _ , pressures = data_sample
+                input = input.unsqueeze(1).to(device)  
+                target = pressures[:, param_idx].unsqueeze(1).unsqueeze(1).to(device)
+        
+                outputs = aggregated_model(input)
+                loss = criterion(outputs, target)
+        
+                valid_loss += loss.item() * input.size(0)
+
+        valid_loss = valid_loss / len(valid_dataloader.dataset)
+
+        logging.info(f"Epoch {epoch+1} / {num_epochs}, Valid Loss: {valid_loss:.4f}")
+
+
+        # Change save directory based on the experiment
+        if (epoch+1) % 2 == 0:
+            model_save_path = f"checkpoints/uci-bp-finetuned/PPGUnet_finetune_{fine_tune_param}_epoch{epoch}.pth"
+            torch.save(model.state_dict(), model_save_path)
+
 
 
 def main(train_dataset="entc"):
@@ -182,6 +270,10 @@ def main(train_dataset="entc"):
         train_ucibp(dataset="ucibp")
     elif train_dataset=="ucibp-npy":
         train_ucibp(dataset="ucibp-npy")
+    elif train_dataset=="ucibp-finetune":
+        train_ucibp_finetune(fine_tune_param="sbp")
+        train_ucibp_finetune(fine_tune_param="dbp")
+        train_ucibp_finetune(fine_tune_param="map")
 
 
 
--- models/fine_tune.py
+++ models/fine_tune.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-class FineTune(nn.Module):
+class FineTuneSBP(nn.Module):
     def __init__(self, in_channels, mid_channels, out_channels, downsampling_factor=2):
         super().__init__()
         
@@ -13,6 +13,42 @@ class FineTune(nn.Module):
             nn.ReLU(),
             nn.BatchNorm1d(out_channels), 
             nn.MaxPool1d(kernel_size=64),
+            nn.Linear(in_features=1, out_features=1, bias=False)
+        )
+
+    def forward(self, x):
+        return self.fine_tune(x)
+
+class FineTuneDBP(nn.Module):
+    def __init__(self, in_channels, mid_channels, out_channels, downsampling_factor=2):
+        super().__init__()
+        
+        self.fine_tune = nn.Sequential(
+            nn.Conv1d(in_channels=in_channels, out_channels=mid_channels, kernel_size=4, stride=4),
+            nn.BatchNorm1d(mid_channels),
+            nn.ReLU(),
+            nn.Conv1d(in_channels=mid_channels, out_channels=out_channels, kernel_size=4, stride=4),
+            nn.ReLU(),
+            nn.BatchNorm1d(out_channels), 
+            nn.MaxPool1d(kernel_size=64),
+            nn.Linear(in_features=1, out_features=1, bias=False)
+        )
+
+    def forward(self, x):
+        return self.fine_tune(x)
+    
+class FineTuneMAP(nn.Module):
+    def __init__(self, in_channels, mid_channels, out_channels, downsampling_factor=2):
+        super().__init__()
+        
+        self.fine_tune = nn.Sequential(
+            nn.Conv1d(in_channels=in_channels, out_channels=mid_channels, kernel_size=4, stride=4),
+            nn.BatchNorm1d(mid_channels),
+            nn.ReLU(),
+            nn.Conv1d(in_channels=mid_channels, out_channels=out_channels, kernel_size=4, stride=4),
+            nn.ReLU(),
+            nn.BatchNorm1d(out_channels), 
+            nn.AvgPool1d(kernel_size=64),
             nn.Linear(in_features=1, out_features=1, bias=False)
         )
 
