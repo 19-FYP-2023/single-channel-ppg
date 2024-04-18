fd6130a760df9f73ba683df0c40fa47f6b458de5
Dumindu-Bandara <dumindubandara1999@gmail.com>
2024-04-18 12:36:43 +0000

    Add FNet train function.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 from torch.utils.data import random_split
 from torch.utils.data import DataLoader
 from models.unet_tranformer import PPGUnet
+from models.fnet_transformer import PPGFNet
 from models.custom_dataloader import ENTCDataset
 from models.custom_dataloader import UCIBPDataset
 from models.custom_dataloader import UCIBPDatasetNPY
@@ -89,6 +90,96 @@ def train_entc():
         if (epoch+1) % 5 == 0:
             model_save_path = f"checkpoints/vatta-pitta/PPGUnet_epoch{epoch}.pth"
             torch.save(model.state_dict(), model_save_path)
+
+def train_ucibp_fnet(dataset="ucibp"):
+
+        logging.info(f"Un-normalized the output")
+        logging.info("Using FNet")
+        logging.info(f"Batch size changed to 128")
+        logging.info(f"Train test split changed form 0.8-0.1-0.1 to 0.6-0.2-0.2")
+        # dataset configuration
+
+        if dataset == "ucibp":
+            UCIBP_dataset = UCIBPDataset("dataset/uci-bp")
+        elif dataset == "ucibp-npy":
+            UCIBP_dataset = UCIBPDatasetNPY("dataset/uci-bp-processed")
+            
+        train_dataset, valid_dataset, test_dataset = random_split(UCIBP_dataset, [0.6, 0.2, 0.2], generator=torch.Generator().manual_seed(42))
+
+        # model configuration
+        num_epochs = 300
+        model = PPGFNet(in_channels=1).to(device)
+        criterion = nn.L1Loss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+        scheduler = ExponentialLR(optimizer, gamma=0.992354)
+
+        # dataloader configuration
+        train_dataloader = DataLoader(train_dataset, batch_size=128, shuffle=True)
+        valid_dataloader = DataLoader(valid_dataset, batch_size=128, shuffle=False)
+        test_dataloader = DataLoader(test_dataset, batch_size=128, shuffle=False)
+
+        for epoch in range(num_epochs):
+            model.train()
+            train_loss = 0.0
+            for i, data_sample in enumerate(train_dataloader):
+                
+                if dataset=="ucibp":
+                    input, target = data_sample
+
+                elif dataset=="ucibp-npy":
+                    input, target, pressures = data_sample
+
+                input = input.unsqueeze(1).to(device)
+                # Divide target by 200 to normalize
+                target = target.unsqueeze(1).to(device)
+
+                optimizer.zero_grad()
+                output = model(input)
+                loss = criterion(output, target)
+                loss.backward()
+                optimizer.step()
+
+                train_loss += loss.item() * input.size(0)
+
+            train_loss = train_loss / len(train_dataloader.dataset)
+
+            logging.info(f"Epoch {epoch+1} / {num_epochs}, Train Loss: {train_loss:.4f}")
+            # print(f"Epoch {epoch+1} / {num_epochs}, Train Loss: {train_loss:.4f}")
+
+            # Adjusting learning rate
+            scheduler.step()
+
+            model.eval()
+
+            valid_loss = 0.0
+
+            with torch.no_grad():
+                for i, data_sample in enumerate(valid_dataloader):
+                    
+                    if dataset=="ucibp":
+                        input, target = data_sample
+
+                    elif dataset=="ucibp-npy":
+                        input, target, pressures = data_sample
+
+                    input = input.unsqueeze(1).to(device)
+                    # Divide target by 200 to normalize
+                    target = target.unsqueeze(1).to(device)
+
+                    output = model(input)
+                    loss = criterion(output, target)
+                    valid_loss += loss.item() * input.size(0)
+
+            valid_loss = valid_loss / len(test_dataloader.dataset)
+            
+            logging.info(f"Epoch {epoch+1} / {num_epochs}, Valid Loss: {valid_loss:.4f}")
+            # print(f"Epoch {epoch+1} / {num_epochs}, Valid Loss: {valid_loss:.4f}")
+
+            # Change save directory based on the experiment
+            if (epoch+1) % 5 == 0:
+                model_save_path = f"checkpoints/ucibp-npy-fnet/PPGUnet_epoch{epoch}.pth"
+                torch.save(model.state_dict(), model_save_path)
+
 
 def train_ucibp(dataset="ucibp"):
 
@@ -278,10 +369,11 @@ def main(train_dataset="entc"):
         train_ucibp_finetune(fine_tune_param="sbp")
         train_ucibp_finetune(fine_tune_param="dbp")
         # train_ucibp_finetune(fine_tune_param="map")
-
+    elif train_dataset=="ucibp-npy-fnet":
+        train_ucibp_fnet(dataset="ucibp-npy")
 
 
 
 if __name__ == "__main__":
     # main(train_dataset="ucibp-finetune")
-    main(train_dataset="ucibp-npy")
+    main(train_dataset="ucibp-npy-fnet")
